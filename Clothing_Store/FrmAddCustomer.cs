a3be67b3570cf438d09208068d1578a0ff1c0ee7
hlrmp <haysjhune@gmail.com>
2023-11-29 00:37:36 +0800

    add

--- Clothing_Store/FrmAddCustomer.cs
+++ Clothing_Store/FrmAddCustomer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,15 @@ namespace Clothing_Store
         private void btnAdd_Click(object sender, EventArgs e) // add button begin
         {
 
+            add();  // add method 
+
+
+            n++;
+
+           
+            MessageBox.Show("Suessfully added" , "New Customer",MessageBoxButtons.OK);
+            this.Hide();
+                
         } // add buttonbend
         protected override void OnPaint(PaintEventArgs e)    // border color begin
         {
@@ -55,6 +65,58 @@ namespace Clothing_Store
 
             }
         } // cancel button end
+
+
+           int n;
+ 
+        public void add()  // add customers 
+        {
+            SqlConnection cnn = new SqlConnection(ConnectionClass.conn);
+
+            string sj = "select count(*)  AS 'Total Customers' from customers";
+            cnn.Open();
+            SqlCommand cmd;
+            cmd = new SqlCommand(sj, cnn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            n = Convert.ToInt32(reader[0]) +1 ;  // - total numbers +1 
+
+
+            cnn.Close();
+
+            string fname = txtFname.Text;
+            string mname = " ";
+            mname = txtMname.Text;
+            string lname = txtLname.Text;
+            string add = txtAddress.Text;
+            string deladd = txtDeliveryAddress.Text;
+            string con = txtContactNo.Text;
+            string em = txtEmail.Text;
+            
+
+
+            cnn.Open();
+            string quer1 = "insert into Customers( Customer_Id, First_Name, Middle_Name, Last_Name, Address, Delivery_Address, Contact_No, Email) values ( @Customer_Id, @First_Name, @Middle_Name, @Last_Name, @Address, @Delivery_Address, @Contact_No, @Email) ";
+            SqlCommand command = new SqlCommand(quer1, cnn);
+
+
+
+            command.Parameters.AddWithValue("@Customer_Id", n);
+            command.Parameters.AddWithValue("@First_Name", fname);
+            command.Parameters.AddWithValue("@Middle_Name", mname);
+            command.Parameters.AddWithValue("@Last_Name", lname);
+            command.Parameters.AddWithValue("@Address", add);
+            command.Parameters.AddWithValue("@Delivery_Address", deladd);
+            command.Parameters.AddWithValue("@Contact_No", con);
+            command.Parameters.AddWithValue("@Email", em);
+
+            command.ExecuteNonQuery();
+
+
+        } // add cutomers 
+
+
 
 
     } // class end
