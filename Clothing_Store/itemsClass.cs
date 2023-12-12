a4198f413e1ed008e8c3775fc91d0511ef5b7ffd
hlrmp <haysjhune@gmail.com>
2023-12-12 13:36:20 +0800

    supplier class and itemsclass

---
+++ Clothing_Store/itemsClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Clothing_Store
+{
+    internal class itemsClass
+    {
+        private string ItemName;
+        private string ItemType;
+        private string Category;
+        private string Size;
+        private int Quantity;
+        private double Price;
+    }
+}
