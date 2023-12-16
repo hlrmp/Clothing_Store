85eb94b1e78ec7f7371f32bfc3cdcb43535fd5f8
hlrmp <haysjhune@gmail.com>
2023-12-16 21:24:27 +0800

    archives

--- Clothing_Store/frmArchives.Designer.cs
+++ Clothing_Store/frmArchives.Designer.cs
@@ -28,12 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbFilter = new System.Windows.Forms.ComboBox();
+            this.lblsearch = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnBackups = new System.Windows.Forms.Button();
+            this.btnOrders = new System.Windows.Forms.Button();
+            this.btnItems = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panelButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbFilter.BackColor = System.Drawing.Color.White;
+            this.cbFilter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(25)))), ((int)(((byte)(82)))));
+            this.cbFilter.FormattingEnabled = true;
+            this.cbFilter.Location = new System.Drawing.Point(700, 41);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(74, 21);
+            this.cbFilter.TabIndex = 48;
+            this.cbFilter.Text = "Filter";
+            // 
+            // lblsearch
+            // 
+            this.lblsearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblsearch.AutoSize = true;
+            this.lblsearch.BackColor = System.Drawing.Color.White;
+            this.lblsearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblsearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(25)))), ((int)(((byte)(82)))));
+            this.lblsearch.Location = new System.Drawing.Point(662, 44);
+            this.lblsearch.Name = "lblsearch";
+            this.lblsearch.Size = new System.Drawing.Size(41, 13);
+            this.lblsearch.TabIndex = 47;
+            this.lblsearch.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Location = new System.Drawing.Point(460, 41);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(243, 20);
+            this.txtSearch.TabIndex = 46;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Matura MT Script Capitals", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(25)))), ((int)(((byte)(82)))));
+            this.lblTitle.Location = new System.Drawing.Point(31, 30);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(299, 39);
+            this.lblTitle.TabIndex = 45;
+            this.lblTitle.Text = "Archives Collections";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(736, 304);
+            this.dataGridView1.TabIndex = 49;
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelButtons.Controls.Add(this.btnItems);
+            this.panelButtons.Controls.Add(this.btnOrders);
+            this.panelButtons.Controls.Add(this.btnBackups);
+            this.panelButtons.Controls.Add(this.btnCustomers);
+            this.panelButtons.Location = new System.Drawing.Point(38, 83);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(736, 34);
+            this.panelButtons.TabIndex = 50;
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCustomers.Location = new System.Drawing.Point(0, 3);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(63, 28);
+            this.btnCustomers.TabIndex = 51;
+            this.btnCustomers.Text = "Customers";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // btnBackups
+            // 
+            this.btnBackups.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnBackups.Location = new System.Drawing.Point(69, 3);
+            this.btnBackups.Name = "btnBackups";
+            this.btnBackups.Size = new System.Drawing.Size(63, 28);
+            this.btnBackups.TabIndex = 52;
+            this.btnBackups.Text = "Backup";
+            this.btnBackups.UseVisualStyleBackColor = true;
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnOrders.Location = new System.Drawing.Point(138, 3);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Size = new System.Drawing.Size(63, 28);
+            this.btnOrders.TabIndex = 53;
+            this.btnOrders.Text = "Orders";
+            this.btnOrders.UseVisualStyleBackColor = true;
+            // 
+            // btnItems
+            // 
+            this.btnItems.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnItems.Location = new System.Drawing.Point(207, 3);
+            this.btnItems.Name = "btnItems";
+            this.btnItems.Size = new System.Drawing.Size(63, 28);
+            this.btnItems.TabIndex = 54;
+            this.btnItems.Text = "Items";
+            this.btnItems.UseVisualStyleBackColor = true;
+            // 
+            // frmArchives
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbFilter);
+            this.Controls.Add(this.lblsearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmArchives";
             this.Text = "frmArchives";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panelButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbFilter;
+        private System.Windows.Forms.Label lblsearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Button btnBackups;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnItems;
+        private System.Windows.Forms.Button btnOrders;
     }
 }
